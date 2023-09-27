5937deadfba2cc86d46adc9435ca9c8caa13322a
Haukiiii <73790219+Haukiiii@users.noreply.github.com>
2023-09-27 09:36:28 +0200

    Update scene.hpp
    Added functions (not done)

--- scene.hpp
+++ scene.hpp
@@ -5,6 +5,13 @@ struct Scene {
 
     std::vector<Polygon*> polygon; 
 
+    void addPolygon(Polygon* pol);
+
+    //TODO add functions for ligthning 
+
+
+    void rayTarget(Ray& ray) const;
+    void traceRay(std::shared_ptr<Ray>& root) const;
 
 
 };
