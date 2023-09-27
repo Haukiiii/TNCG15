7a6212377db58a9969f47ff27e854e1b2e043b3b
KnetusL <kenla433@student.liu.se>
2023-09-27 02:09:29 +0200

    Implementerat möller trumbore-kenton
    Gjort rayIntersection metoder for triangle och rectangle, rectangle är dock inte helt säker på om den är korrekt

--- Polygon.cpp
+++ Polygon.cpp
@@ -12,8 +12,7 @@ Triangle::Triangle(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3
     normal = glm::normalize(glm::cross(edge2, edge1));
 }
 
-Rectangle::Rectangle(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3, const glm::vec3& v4, const Material* material) : Polygon { material }{
-   
+Rectangle::Rectangle(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3, const glm::vec3& v4, const Material* material) : Polygon { material } {
     //set verticies
     vertices[0] = v1;
 	vertices[1] = v2;
@@ -36,4 +35,62 @@ glm::vec3 Triangle::CalcUnitNormal(const glm::vec3& hit)
 glm::vec3 Rectangle::CalcUnitNormal(const glm::vec3& hit)
 {
 	return glm::normalize(glm::cross(this->edge1, this->edge2));
+}
+
+//Ray intersections implementations (möller trumbore)
+float Triangle::rayIntersection(Ray* ray){ //lecture 4 möller trumbore 
+
+    //check if intersection is possible (scalar product between raydirection and surface normal must be negative)
+    if(glm::dot(ray->direction, normal) > 0) 
+    {
+        return -1.0; //no intersection possible
+    }
+
+    //define T, P, Q, already have D, E1, E2
+    glm::vec3 T = ray->startpoint - vertices[0];
+    glm::vec3 P = glm::cross(ray->direction, edge2);
+    glm::vec3 Q = glm::cross(T, edge1); 
+
+    //calculate barycentric coordinates of the intersection point
+    glm::vec3 intersection = (1 / glm::dot(P, edge1)) * glm::vec3(glm::dot(Q, edge2), glm::dot(P, T), glm::dot(Q, ray->direction)); 
+
+    //check if the intersection point lies within the triangle
+    if(intersection.x < 0.0f || intersection.y < 0.0f || intersection.z < 0.0f || intersection.z + intersection.y > 1.0f) 
+    {
+        return -1.0f; //no intersection
+    }
+    //ray intersects triangle
+    return intersection.x; //return the parameter (t) value along the ray where the intersection occurs
+}
+
+float Rectangle::rayIntersection(Ray* ray){ //************OSÄKER PÅ DENNA*************
+
+    // Check if intersection is possible
+    if (glm::dot(ray->direction, normal) > 0.0f) {
+        return -1.0f; // No intersection possible
+    }
+
+    // Define T, P, Q, already have D, E1, E2
+    glm::vec3 T = ray->startpoint - vertices[0];
+    glm::vec3 P = glm::cross(ray->direction, edge2);
+    glm::vec3 Q = glm::cross(T, edge1);
+
+    // Calculate barycentric coordinates of the intersection point
+    glm::vec3 intersection = (1.0f / glm::dot(P, edge1)) * glm::vec3(glm::dot(Q, edge2), glm::dot(P, T), glm::dot(Q, ray->direction));
+
+    // Check if the intersection point is within the first triangle (ABD)
+    if (intersection.x >= 0.0f && intersection.y >= 0.0f && intersection.z >= 0.0f && intersection.z + intersection.y <= 1.0f) {
+        return intersection.x; // Return the parameter (t) value along the ray where the intersection occurs
+    }
+
+    // Check if the intersection point is within the second triangle (BCD)
+    glm::vec3 T2 = ray->startpoint - vertices[1];
+    Q = glm::cross(T2, edge2);
+    intersection = (1.0f / glm::dot(P, edge2)) * glm::vec3(glm::dot(Q, edge3), glm::dot(P, T2), glm::dot(Q, ray->direction));
+
+    if (intersection.x >= 0.0f && intersection.y >= 0.0f && intersection.z >= 0.0f && intersection.z + intersection.y <= 1.0f) {
+        return intersection.x; // Return the parameter (t) value along the ray where the intersection occurs
+    }
+
+    return -1.0f; // No intersection in both triangles
 }
--- Polygon.hpp
+++ Polygon.hpp
@@ -10,6 +10,8 @@ struct Polygon
     Polygon() = default; //default constuctor, creates a black material with 0 absoption and 0 emittance
 
     virtual glm::vec3 CalcUnitNormal(const glm::vec3& hit); //method to compute unit normal, will be overrided in subclasses
+
+    virtual float rayIntersection(Ray* ray); //method to calculate ray intersection with rect/triangle, overrides in both subclasses.
 };
 
 
@@ -24,6 +26,8 @@ struct Triangle: public Polygon
     Triangle(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3, const Material* material);
 
     glm::vec3 CalcUnitNormal(const glm::vec3& hit) override;
+
+    float rayIntersection(Ray* ray) override; //method for calulating ray intersection with triangle, returns the parameter t which determines a point on a ray
 };
 
 struct Rectangle: public Polygon
@@ -38,4 +42,6 @@ struct Rectangle: public Polygon
     Rectangle(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3, const glm::vec3& v4, const Material* material);
     
     glm::vec3 CalcUnitNormal(const glm::vec3& hit) override;
+
+    float rayIntersection(Ray* ray) override; //method for calulating ray intersection with rectangle, returns the parameter t which determines a point on a ray
 };
