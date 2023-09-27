92a842fb681f82b900cab5d8ecd726bfa728cedc
Haukiiii <73790219+Haukiiii@users.noreply.github.com>
2023-09-27 09:38:19 +0200

    Added depth and is_leaf

--- ray.hpp
+++ ray.hpp
@@ -13,6 +13,9 @@ Polygon* target; //used to store information about the object or surface that th
 std::shared_ptr<Ray> prev; //ptr to previous ray
 std::shared_ptr<Ray> next; //ptr to next ray
 
+int depth{ 0 }; // check the depth of tree
+bool is_leaf{ false }; // if the ray is the last
+
 //constructors
 Ray() = default;
 Ray(glm::vec3 startpoint, glm::vec3 endpoint);
